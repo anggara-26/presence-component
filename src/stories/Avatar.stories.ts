f7b87f03549d30b57d27c36cac49d144bdd8e3a2
Anggara <anggara.roshandi.putra5@gmail.com>
2023-09-02 10:53:21 +0700

    change avatar pictures

--- src/stories/Avatar.stories.ts
+++ src/stories/Avatar.stories.ts
@@ -24,7 +24,7 @@ type Story = StoryObj<typeof meta>;
 // More on writing stories with args: https://storybook.js.org/docs/react/writing-stories/args
 export const Photo: Story = {
   args: {
-    src: "https://e1.pxfuel.com/desktop-wallpaper/225/102/desktop-wallpaper-pin-on-anime-girl-cute-anime-girl-profile-thumbnail.jpg",
+    src: "https://yt3.googleusercontent.com/1DLrc_XiE_1FwD-scIWDjMXRXh637U91TGn5qUS3sFC56P-vUFvjHttshe3fFwQATp_0ng5Y7U4=s900-c-k-c0x00ffffff-no-rj",
     name: "Anggara",
   },
 };
